3ccd88b71a9b9d3565436d8cbc2ef631618d182e
Thobias Stahlschmidt <thobias0@gmail.com>
2024-06-04 19:24:57 -0300

    Add example of usage of external lib

--- app/main.cc
+++ app/main.cc
@@ -1,11 +1,17 @@
 #include <iostream>
 #include "my_lib.h"
 #include "config.hpp"
+#include "nlohmann/json.hpp"
 
 int main(void)
 {
   std::cout << project_name << "\n";
   std::cout << project_version << "\n";
+  std::cout << "JSON Lib Version:" 
+    << NLOHMANN_JSON_VERSION_MAJOR << "."
+    << NLOHMANN_JSON_VERSION_MINOR << "."
+    << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
   print_hello_world();
   return 0;
 }
