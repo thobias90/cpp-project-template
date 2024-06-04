540287a254cfe716796a9c890bc0765258e2abb8
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-06-04 18:48:27 -0300

    Add print of the variables into main

--- app/main.cc
+++ app/main.cc
@@ -1,7 +1,11 @@
+#include <iostream>
 #include "my_lib.h"
+#include "config.hpp"
 
 int main(void)
 {
+  std::cout << project_name << "\n";
+  std::cout << project_version << "\n";
   print_hello_world();
   return 0;
 }
