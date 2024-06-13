2b46bccb2c71eba5ae2d5181b8ee8456cb5601e9
Thobias Stahlschmidt <thobias0@gmail.com>
2024-06-13 20:38:01 -0300

    Add unit testing integration

--- src/my_lib/my_lib.cc
+++ src/my_lib/my_lib.cc
@@ -6,14 +6,19 @@
 
 /**
  * @brief Print hello world and tests the json lib
- * 
+ *
  */
 void print_hello_world()
 {
   std::cout << "Hello world\n";
 
-  std::cout << "JSON Lib Version:" 
-    << NLOHMANN_JSON_VERSION_MAJOR << "."
-    << NLOHMANN_JSON_VERSION_MINOR << "."
-    << NLOHMANN_JSON_VERSION_PATCH << "\n";
+  std::cout << "JSON Lib Version:"
+            << NLOHMANN_JSON_VERSION_MAJOR << "."
+            << NLOHMANN_JSON_VERSION_MINOR << "."
+            << NLOHMANN_JSON_VERSION_PATCH << "\n";
+}
+
+std::uint32_t factorial(std::uint32_t number)
+{
+  return number <= 1 ? 1 : factorial(number - 1) * number;
 }
